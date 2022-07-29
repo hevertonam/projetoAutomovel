23dc037362da56dad0c8506bb3b2a6cfd04dd10e
Heverton <hevertonam2018@gmail.com>
2022-07-29 15:48:36 -0300

    criado class Specification e separando modulos

---
+++ src/modules/cars/model/Specification.ts
@@ -0,0 +1,18 @@
+import {v4 as uuidv4} from 'uuid' 
+
+class Specification {
+    id?: string;
+        nome: string;
+        descricao: string;
+        data_criacao: Date;
+
+
+       constructor(){
+       if(!this.id){
+              this.id = uuidv4();
+       }
+ 
+       }
+      
+}
+export {Specification };

79f151d2fa3383f4f0d411f1b1759e8d7ae47ff0
Heverton <hevertonam2018@gmail.com>
2022-08-03 12:08:50 -0300

    refatorando as rotas e adicionado arquivo upload

--- src/modules/cars/model/Category.ts
+++ src/modules/cars/model/Category.ts
@@ -2,22 +2,16 @@ import {v4 as uuidv4} from 'uuid'
 
  class Category
   {
- 
-        id?: string;
+         id?: string;
         nome: string;
         descricao: string;
         data_criacao: Date;
 
-
        constructor(){
        if(!this.id){
               this.id = uuidv4();
-       }
- 
-       }
-      
- 
+            }
+        }
        }  
-
 
  export  {Category};
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,18 @@
 import {Router} from 'express';
+import multer from 'multer';
 
 import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategories';
 
 
+
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+
+});
 
 const categoriesRepository = CategoriesRepository.getInstance();
 
@@ -17,6 +24,13 @@ categoriesRoutes.post("/", (request, response) => {
 categoriesRoutes.get("/", (request, response) => {
    return listCategoryController.handle(request,response);
    
+});
+
+categoriesRoutes.post ("/import", upload.single("file"), (request, response) => {
+  const {file} = request;
+  console.log(file);
+  return response.send();
+  
 });
 
 export {categoriesRoutes};

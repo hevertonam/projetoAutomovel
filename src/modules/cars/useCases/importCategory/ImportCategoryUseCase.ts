993d433ccda0d1c28738b3b0d6760d7867aef9a1
Heverton <hevertonam2018@gmail.com>
2022-08-05 16:10:24 -0300

    incluindo strem

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,11 +1,39 @@
+import fs from "fs";
+import csvParse from "csv-parse";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
-
+interface IImportCategory{
+    nome: string;
+    descricao: string;
+}
 
 class ImportCategoryUseCase{
 
-    execute(file:any){
-    console.log(file);
+    constructor(private categoriesRepository:ICategoriesRepository){}
+
+
+
+    loadCategories(file: Express.Multer.File):IImportCategory[]{
+        const stream = fs.createReadStream(file.path); 
+        const categories: IImportCategory[]=[];
+              const parseFile = csvParse();
+
+        stream.pipe(parseFile);
+
+        parseFile.on("data",  async(line)=>{
+            const [nome, descricao] = line;
+            categories.push({
+                nome,
+                descricao,
+            });
+        });
+        return categories;
+    }
+
+    execute(file: Express.Multer.File):void{
+        const categories = this.loadCategories(file);
+        console.log(categories);
     }
 }
-
+ 
 export{ImportCategoryUseCase}
